530c3bbe8584762b0430e817928e969f9f1c9ead
Nurul <nurulakter8@gmail.com>
2020-10-07 01:46:38 -0500

    signIn commit

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  static const routeName = '/signInScreen';
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInState();
+  }
+}
+
+class _SignInState extends State<SignInScreen> {
+  _Controller con;
+
+  @override
+  void initState() {
+    super.initState();
+    con= _Controller(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('s'),
+      ),
+      body: Text('b'),
+    );
+  }
+}
+
+class _Controller {
+  _SignInState _state;
+  _Controller(this._state);
+}
